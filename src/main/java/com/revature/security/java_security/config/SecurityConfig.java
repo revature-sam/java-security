aa19245f0c7aac9ee2b07ef886ece7cedb06c801
sam-gupta-git <sam78748@gmail.com>
2025-10-16 06:53:25 -0500

    oauth2 and other fixes

--- src/main/java/com/revature/security/java_security/config/SecurityConfig.java
+++ src/main/java/com/revature/security/java_security/config/SecurityConfig.java
@@ -2,10 +2,18 @@ package com.revature.security.java_security.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties.Authentication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +24,39 @@ public class SecurityConfig {
             .csrf(customizer -> customizer.disable()) // Disable CSRF for simplicity
             .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers("/public/**").permitAll() // Public endpoints
-                .anyRequest().authenticated() // All other endpoints require authentication
+                .anyRequest().authenticated()) // All other endpoints require authentication
                 .formLogin(Customizer.withDefaults()) // Use form-based authentication
-                .httpBasic(Customizer.withDefaults())
-            ); // Use basic authentication
+                .httpBasic(Customizer.withDefaults());
+             // Use basic authentication
 
         return http.build();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChainOAuth(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            .oauth2Login(Customizer.withDefaults())
+            .oauth2Client(Customizer.withDefaults());
+            
+
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService  userDetailsService() {
+        UserDetails user = User
+            .withUsername("sam")
+            .password("gupta")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(new BCryptPasswordEncoder());
+        return authProvider;
     }
 }
